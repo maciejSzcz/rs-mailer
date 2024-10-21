cf2787eb0cb407656e87bec0c968da564d86652b
Maciej Szczęsny <szczesnymcj@gmailc.om>
2024-10-21 22:07:56 +0200

    Initial project structure and functionality

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::{io::Result, net::TcpListener};
+
+use sqlx::PgPool;
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    run(listener, connection_pool)?.await
+}
